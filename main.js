f7aaaa0e5df95773f1b74736fd7f626e646836ee
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2017-12-29 00:44:17 -0600

    Add Capture snapshot button, canvas element

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var videoDevices = [];
 var video = document.querySelector('video');
-var toggleCameraButton = document.querySelector('button');
+var toggleCameraButton = document.querySelector('#toggle-camera-button');
+var captureSnapshotButton = document.querySelector('#capture-snapshot-button');
+var canvas = window.canvas = document.querySelector('canvas');
 var videoIndex = 0;
+
+canvas.width = 480;
+canvas.height = 360;
 
 function handleSuccess(stream) {
   window.stream = stream;
@@ -42,4 +47,11 @@ navigator.mediaDevices.enumerateDevices().then(function (deviceInfos) {
 toggleCameraButton.onclick = function () {
   videoIndex = (videoIndex + 1) % videoDevices.length;
   startStream();
+};
+
+captureSnapshotButton.onclick = function () {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').
+    drawImage(video, 0, 0, canvas.width, canvas.height);
 };
